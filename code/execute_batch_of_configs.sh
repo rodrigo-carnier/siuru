1267ad3547765a9ffdf490f1900732f0124624a7
Rodrigo Carnier (NII) <rodrigo_carnier@nii.ac.jp>
2025-01-23 20:01:06 +0000

    (Publishable results finally!) Configuration files for the 12 comparisons between RF,ARF,Hoeff,NBGauss,NBComplement methods using datasets Edge, MQTTset, MQTT IoT.
    Also reorganized the previous configuration files into category folders.

--- code/execute_batch_of_configs.sh
+++ code/execute_batch_of_configs.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Check if a folder is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <folder>"
-    exit 1
+# Check if a folder path is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <folder_path>"
+  exit 1
 fi
 
-# Assign the folder parameter to a variable
-folder="$1"
+# Assign the folder path
+FOLDER_PATH="$1"
 
-# Loop through all files in the specified folder
-for file in "$folder"/*; do
-    if [ -f "$file" ]; then
-        echo "Processing file: $file"
-        python3 code/IoT-AD-stream.py -c "$file"
-    fi
+# Verify if the provided path is a directory
+if [ ! -d "$FOLDER_PATH" ]; then
+  echo "Error: $FOLDER_PATH is not a directory."
+  exit 1
+fi
+
+# Iterate over each file in the folder
+for file in "$FOLDER_PATH"/*; do
+  if [ -f "$file" ]; then
+    echo "Processing file: $file"
+    python3 code/IoT-AD-stream.py -c "$file"
+  fi
 done
+
